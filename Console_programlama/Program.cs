348e684d75ad18b65fda7b7fb406c7b93c324141
muzeyyenyildiz <mim.mzyn@gmail.com>
2021-12-23 11:44:49 +0300

    first commit

---
+++ Console_programlama/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("İsminizi giriniz:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyisminizi giriniz:");
+string Surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + Surname + "!");
