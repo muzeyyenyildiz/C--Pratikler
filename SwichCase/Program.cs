ac5c236cc589f90082c16f95f2dbc168b9a05bf3
muzeyyenyildiz <mim.mzyn@gmail.com>
2021-12-27 15:32:32 +0300

    SwichCase added

---
+++ SwichCase/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int month = DateTime.Now.Month;
+
+//Expression bodies
+switch (month)
+{
+    case 1: Console.WriteLine("January");
+    break;
+    case 2: Console.WriteLine("February"); 
+    break;
+    case 3: Console.WriteLine("March");
+    break;
+    case 4: Console.WriteLine("April");
+    break;
+    case 5: Console.WriteLine("May");
+    break;
+    case 6: Console.WriteLine("June");
+    break;
+    case 7: Console.WriteLine("july");
+    break;
+    case 8: Console.WriteLine("August");
+    break;
+    case 9: Console.WriteLine("September");
+    break;
+    case 10: Console.WriteLine("October");
+    break;
+    case 11: Console.WriteLine("November");
+    break;
+    case 12: Console.WriteLine("December");
+    break;
+    default:
+    break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+    Console.WriteLine("Winter");
+    break;
+
+    case 3:
+    case 4:
+    case 5:
+    Console.WriteLine("Spring");
+    break;
+
+    case 6:
+    case 7:
+    case 8:
+    Console.WriteLine("Summer");
+    break;
+
+    case 9:
+    case 10:
+    case 11:
+    Console.WriteLine("Autum");
+    break;
+
+    default:
+    break;
+}
